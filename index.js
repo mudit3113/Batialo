45429ca706f210ecc97510f53bb1ab437a99d94d
mudit3113 <116980345+mudit3113@users.noreply.github.com>
2023-04-18 01:05:29 +0530

    chat-engine completed

--- index.js
+++ index.js
@@ -3,23 +3,30 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
+
 //used for session cookie
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const passportJWT = require('./config/passport-jwt-strategy');
 const passportGoogle = require('./config/passport-google-oauth2-strategy');
+
+const app  = express();
+
+const port = 8000;
+
 const MongoStore = require('connect-mongo')(session);
 const saasMiddleware = require('node-sass-middleware');
 const bodyParser = require('body-parser');
 const flash = require('connect-flash');
 const customMware = require('./config/middleware');
 
+//setup the chat server to be used with socket.io
+const chatServer = require('http').Server(app);
+const chatSockets = require('./config/chat_sockets').chatSockets(chatServer);
+chatServer.listen(5000);
+console.log("chat server is listening on port 5000");
 
-
-const app  = express();
-
-const port = 8000;
 
 
 app.use(saasMiddleware({
